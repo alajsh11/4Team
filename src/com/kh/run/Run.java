8f41ef55b7e9acbd5b449510eb88e291a01c6c1c
alajsh11 <alajsh11@naver.com>
2021-05-07 13:27:51 +0900

    test2

--- src/com/kh/run/Run.java
+++ src/com/kh/run/Run.java
@@ -3,8 +3,8 @@ package com.kh.run;
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("HelloWorld");
+		System.out.println("Hi World");
 	}
 
 }
