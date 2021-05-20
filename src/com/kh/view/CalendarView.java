04c2ad4502c383e0766b83077057f93479a2ec09
yunjeong17 <var0701@naver.com>
2021-05-20 13:55:00 +0900

    캘린더패널 삭제/ 기타 수정

--- src/com/kh/view/CalendarView.java
+++ src/com/kh/view/CalendarView.java
@@ -8,16 +8,11 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.GregorianCalendar;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -25,8 +20,12 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
+
+import com.kh.controller.CalendarController;
+import com.kh.controller.DiaryInformationController;
+import com.kh.view.DiaryWriteView;
+
 
 public class CalendarView {
 	// new Color(0xddc6e6)
@@ -48,7 +47,18 @@ public class CalendarView {
 	private JPanel ham;
 	private JFrame jf;
 	
-	public CalendarView() {
+	private String userId;
+	
+	private CalendarController cc = new CalendarController();
+	private JButton[] month;
+	private int loW; // 해당 월 1일의 요일을 받기위한 변수
+	private int[] lastDate = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+
+	public CalendarView() {}
+	public CalendarView(String uId) {
+		System.out.println(uId);
+		userId=uId;
 		Font font = new Font("맑은 고딕", Font.PLAIN, 18);
 		jf = new JFrame();
 		jf.setBounds(0, 0, 640, 960);
@@ -74,7 +84,7 @@ public class CalendarView {
 		nowYear = gc.get(GregorianCalendar.YEAR);
 		nowMonth = gc.get(GregorianCalendar.MONTH);
 		nowDate = gc.get(GregorianCalendar.DATE);
-		calendar = new CalenderPanel(nowYear, nowMonth, nowDate);
+		calendar = createCalendarPanel(nowYear, nowMonth, nowDate,userId);
 
 		// 검색 버튼 및 엔터 시
 		ImageIcon icon2 = new ImageIcon("image/search.PNG");
@@ -121,7 +131,7 @@ public class CalendarView {
 				String selectedYear = selected.substring(0, 4);
 				nowYear = Integer.parseInt(selectedYear);
 				jf.remove(calendar);
-				calendar = new CalenderPanel(nowYear, nowMonth, nowDate);
+				calendar = createCalendarPanel(nowYear, nowMonth, nowDate,userId);
 				
 				jf.add(calendar, BorderLayout.CENTER);
 				jf.add(ham);
@@ -145,7 +155,7 @@ public class CalendarView {
 				int selectedIndex = monCombo.getSelectedIndex();
 				nowMonth = selectedIndex;
 				jf.remove(calendar);
-				calendar = new CalenderPanel(nowYear, nowMonth, nowDate);
+				calendar = createCalendarPanel(nowYear, nowMonth, nowDate,userId);
 				jf.add(calendar, BorderLayout.CENTER);
 				jf.add(ham);
 				jf.add(panel);
@@ -167,41 +177,86 @@ public class CalendarView {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	public class HintTextField extends JTextField {
-		Font font = new Font("맑은 고딕", Font.PLAIN, 20);
+	public JPanel createCalendarPanel(int y,int m, int d,String uId) {
+		isLeapYear(y); // 2월 윤년이면 날짜배열 세팅 바꾸기
+		JPanel calendar = new JPanel();
+		Color calendarBack = new Color(0xeeeeee);
+		month = new JButton[42];
 
-		public HintTextField(final String hint) {
-			setName("searchTxt");
-			setText(hint);
-			setFont(font);
-			setForeground(Color.GRAY);
-			this.addFocusListener(new FocusAdapter() {
-				@Override
-				public void focusGained(FocusEvent e) {
-					if (getText().equals(hint)) {
-						setText("");
-						setForeground(Color.BLACK);
-					} else {
-						setText(getText());
-					}
-				}
+		JPanel[] emptyPanel = new JPanel[14];
+		JLabel[] date = new JLabel[49];
+		JPanel[] doW = new JPanel[7]; // (일~토)요일 텍스트 패널에 표시
+		
+		nowYear = y;
+		nowMonth = m;
+		nowDate = d;
+		System.out.println(nowYear+" "+nowMonth+" "+nowDate);
+		
+		gc.set(nowYear, nowMonth, 1);
+		loW = gc.get(GregorianCalendar.DAY_OF_WEEK); // 해당 월 1일의 요일을 받기위한 변수
+		// 캘린더 흰색
+		calendar.setBounds(2, 300, 620, 500);
+		calendar.setLayout(new GridLayout(0, 7, 1, 1));
+		calendar.setBackground(new Color(0xeeeeee));
 
-				@Override
-				public void focusLost(FocusEvent e) {
-					if (getText().equals(hint) || getText().length() == 0) {
-						setText(hint);
-						setForeground(Color.GRAY);
-					} else {
-						setText(getText());
-						setForeground(Color.BLACK);
-					}
-				}
-			});
+		String[] dayOfWeek = { "일", "월", "화", "수", "목", "금", "토" };
+		// 캘린더 요일 표시
+		for (int i = 0; i < 7; i++) {
+			date[i] = new JLabel();
+			date[i].setFont(new Font("Serif", Font.PLAIN, 20));
+			date[i].setHorizontalAlignment(JLabel.CENTER);
+
+			doW[i] = new JPanel();
+			doW[i].setLayout(new BorderLayout());
+
+			date[i].setText(dayOfWeek[i]);
+			doW[i].setBackground(new Color(0xdddddd));
+
+			doW[i].add(date[i]);
+			calendar.add(doW[i]);
 		}
+
+		int cnt = 0;
+		// 캘린더 날짜 표시
+		for (int i = 0; i < 42; i++) {
+			month[i] = new JButton();
+			month[i].setLayout(new BorderLayout());
+			if ((i < lastDate[nowMonth] + (loW - 1)) && (i >= loW - 1)) { // 날짜세팅
+//				System.out.println(" i: " + i);
+				date[i] = new JLabel();
+				date[i].setFont(new Font("Serif", Font.PLAIN, 20));
+				date[i].setHorizontalAlignment(JLabel.CENTER);
+				date[i].setText(Integer.toString(i - loW + 2));
+				month[i].addActionListener(new BtnActionListner());
+				month[i].add(date[i]);
+			}
+			if (i < loW - 1) { // 앞에 빈 날짜
+				emptyPanel[cnt] = new JPanel();
+				emptyPanel[cnt].setBackground(calendarBack);
+				calendar.add(emptyPanel[cnt]);
+			} else if (i <= lastDate[nowMonth] + loW - 2) { // 말일이 있는 주까지 세팅
+				month[i].setBackground(Color.white);
+
+				calendar.add(month[i], BorderLayout.EAST);
+			}
+
+		}
+		
+		return calendar;
 	}
 
 
+	// 윤년확인 메소드
+	public void isLeapYear(int year) {
+		if ((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0) {
+			lastDate[1]=29;
+		}
+		else {
+			lastDate[1] = 28;
+		}
 
+	}
+	
 	class BtnActionListner implements ActionListener {
 
 		@Override
@@ -216,6 +271,13 @@ public class CalendarView {
 			}
 			if (e.getSource() == infoBtn) {
 				System.out.println("내정보로 이동");
+			}
+			
+			for (int i = loW - 1; i < lastDate[gc.get(GregorianCalendar.MONTH)]+ (loW - 1); i++) {
+				if (e.getSource() == month[i]) {
+					jf.setVisible(false);
+					new DiaryInformationController().diaryRead("01023905227/21.05.18.txt");
+				}
 			}
 		}
 	}
