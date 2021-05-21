2bd436fe3262a11bcd1f2feca7ae780cc212d1d0
alajsh11 <alajsh11@naver.com>
2021-05-21 09:23:54 +0900

    다시보냅니다ㅜ

--- src/com/kh/model/dao/UserDao.java
+++ src/com/kh/model/dao/UserDao.java
@@ -8,28 +8,29 @@ import java.util.StringTokenizer;
 
 import com.kh.model.vo.User;
 
-public class UserDao {
+public class UserDao {	// User.dat 이용해서 User 객체화
+	
 	ArrayList<User> list = new ArrayList<User>();
 
-	public void print1() {
-		for(int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
-		}
-
+	public ArrayList<User> getList() {	//list 반환 (getter)
+		return list;
 	}
 
-	public void userList() {
+	
+	public void userListUpdate() {	//User.dat파일 정보 이용해서 list객체화 메소드
 		FileReader fw = null;
 		StringTokenizer st;
 		String a = "";
 
 		try {
+			// User.dat 내용 String a에 넣기
 			fw = new FileReader("User.dat");
 			int value = 0;
 			while ((value = fw.read()) != -1) {
 				a += String.valueOf((char) value);
 			}
-
+			
+			// String a 내용 한줄씩 나눈후 arr[] 배열에 대입
 			st = new StringTokenizer(a, "\n");
 			String[] arr = new String[st.countTokens()];
 
@@ -37,15 +38,16 @@ public class UserDao {
 			while (st.hasMoreTokens()) {
 				arr[value++] = st.nextToken();
 			}
-
+			
+			
+			// arr[] 배열 길이만큼 반복시켜 list 추가하기
 			for (int listSize = 0; listSize < arr.length; listSize++) {
 				String id = "";
 				String pwd = "";
 				String pwdAnswer = "";
 				String no = "";
 				
-				
-				
+				//arr[listSize] 에 , 기준으로 나눈후 strArr[]에 대입
 				st = new StringTokenizer(arr[listSize], ",");
 				String[] strArr = new String[st.countTokens()];
 				value = 0;
@@ -53,23 +55,29 @@ public class UserDao {
 					strArr[value++] = st.nextToken();
 					
 				}
-
+				
+				//유저 번호 값
 				for (int i = 5; i < strArr[1].length(); i++) {
 					no += strArr[1].charAt(i);
 				}
-
+				
+				//유저 아이디 값
 				for (int i = 5; i < strArr[2].length() - 1; i++) {
 					id += strArr[2].charAt(i);
 				}
-
+				
+				//유저 비밀번호 값
 				for (int i = 6; i < strArr[3].length() - 1; i++) {
 					pwd += strArr[3].charAt(i);
 				}
-
-				for (int i = 12; i < strArr[4].length() - 1; i++) {
+				
+				//유저 비밀번호 힌트값
+				for (int i = 12; i < strArr[4].length() - 2; i++) {
 					pwdAnswer += strArr[4].charAt(i);
 				}
-
+				
+				
+				//총 일기 계수 구하기
 				String dc = strArr[0];
 				st = new StringTokenizer(dc, "=");
 
@@ -79,13 +87,13 @@ public class UserDao {
 					dc = st.nextToken();
 
 				}
-				int dCount = Integer.parseInt(dc);
-
+				int dCount = Integer.parseInt(dc);	// 일기계수 int 형변환
+				
+				// 위 값을 이용해 list에 넣을  uUser 생성
 				User uUser = new User(id, pwd, pwdAnswer);
 				uUser.setDiaryCount(dCount);
 				uUser.setuNo(no);
-				// System.out.println(uUser);
-				list.add(uUser);
+				list.add(uUser);	// 리스트 추가
 			}
 
 		} catch (FileNotFoundException e) {
