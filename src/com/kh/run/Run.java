bf90c7a1e2fae49b339a8e9f1e6ec83cc6251f0f
Kim sowon <nare20027@gmail.com>
2021-05-23 16:04:30 +0900

    끝이었으면 좋겠다!

--- src/com/kh/run/Run.java
+++ src/com/kh/run/Run.java
@@ -1,12 +1,12 @@
 package com.kh.run;
 
-import com.kh.controller.CalendarController;
+import com.kh.view.LoginView;
 
 public class Run {
 
 	public static void main(String[] args) {
-		new CalendarController("01023905226");
-		
+		 //new CalendarController("01023905226");
+		new LoginView();
 	
 
 	}
